57149ffdce5f8e7fe4f582050b660ea97c5b782f
BUD93 <burhan.arshad93@gmail.com>
2018-07-09 16:07:15 +0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
-Copy-Item -Path "C:\Program Files (x86)\Jenkins\workspace\test\target\my-app-1.0-SNAPSHOT.jar" -Destination "C:\Program Files\jboss\bin\deployments\my-app-1.0-SNAPSHOT.jar"
+Copy-Item -Path "C:\Program Files (x86)\Jenkins\workspace\numberguess\target\jboss-numberguess.war" -Destination "C:\Program Files\jboss\bin\deployments\jboss-numberguess.war"
 cd "C:\Program Files\jboss\bin"
-./jboss-cli.ps1 --connect --controller=remote+http://127.0.0.1:9990 --user=admin --password=bursys@1234 --command="deploy /deployments/my-app-1.0-SNAPSHOT.jar"
+./jboss-cli.ps1 --connect --controller=remote+http://127.0.0.1:9990 --user=admin --password=bursys@1234 --command="deploy /deployments/jboss-numberguess.war"
