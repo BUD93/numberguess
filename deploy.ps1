a98ea54a604aa058a884ca6cdd36dbabcb65d9b8
BUD93 <burhan.arshad93@gmail.com>
2018-07-17 13:40:07 +0500

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
 Copy-Item -Path "C:\Program Files (x86)\Jenkins\workspace\numberguess\target\jboss-numberguess.war" -Destination "C:\Program Files\jboss\bin\deployments\jboss-numberguess.war"
 cd "C:\Program Files\jboss\bin"
-./jboss-cli.ps1 --connect --controller=remote+http://23.96.24.10:9990 --user=admin --password=bursys@1234 --command="deploy /deployments/jboss-numberguess.war"
+./jboss-cli.ps1 --connect --controller=remote+http://23.96.24.10:9990 --user=admin --password=abcd@1234 --command="deploy /deployments/jboss-numberguess.war"
